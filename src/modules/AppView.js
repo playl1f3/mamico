e3827a81c9080cb0a704d56a285e8ad1807767e6
wee khen lai <weekhen@gmail.com>
2017-02-16 22:38:42 -0500

    side menu changes

--- src/modules/AppView.js
+++ src/modules/AppView.js
@@ -1,11 +1,16 @@
-import React, {PropTypes} from 'react';
-import {View, StyleSheet, ActivityIndicator} from 'react-native';
+import React, { PropTypes } from 'react';
+import { View, StyleSheet, ActivityIndicator, Image } from 'react-native';
 import NavigationViewContainer from './navigation/NavigationViewContainer';
 import * as auth0 from '../services/auth0';
 import * as snapshotUtil from '../utils/snapshot';
 import * as SessionStateActions from '../modules/session/SessionState';
 import store from '../redux/store';
 import DeveloperMenu from '../components/DeveloperMenu';
+import {
+  List,
+  ListItem,
+  SideMenu
+} from 'react-native-elements';
 
 const AppView = React.createClass({
   propTypes: {
@@ -39,24 +44,78 @@ const AppView = React.createClass({
   },
 
   render() {
+    const src = require('../../assets/nidec_logo.png');
+
+    const list = [
+      {
+        name: 'Home',
+        icon: 'home'
+      },
+      {
+        name: 'Call Support',
+        icon: 'call'
+      },
+      {
+        name: 'Email Support',
+        icon: 'email'
+      },
+      {
+        name: 'Terms and Condition',
+        icon: 'assignment'
+      }
+    ];
+
+    const MenuComponent = (
+      <View style={{flex: 1, backgroundColor: '#ededed', paddingTop: 30}}>
+        <View style={{backgroundColor: 'darkgrey', justifyContent: 'center', alignItems: 'center', alignSelf: 'center'}}>
+          <Image
+            source={src}
+            style={styles.logo} />
+        </View>
+        <List containerStyle={{marginBottom: 20}}>
+          {
+            list.map((l, i) => (
+              <ListItem
+                roundAvatar
+                onPress={() => console.log('something')}
+                key={i}
+                title={l.name}
+                leftIcon={{name: l.icon}}
+              />
+            ))
+          }
+        </List>
+      </View>
+    );
+
+
+
     if (!this.props.isReady) {
       return (
         <View>
-          <ActivityIndicator style={styles.centered}/>
+          <ActivityIndicator style={styles.centered} />
         </View>
       );
     }
 
     return (
-      <View style={{flex: 1}}>
-        <NavigationViewContainer />
-        {__DEV__ && <DeveloperMenu />}
-      </View>
+      <SideMenu
+        isOpen={true}
+        menu={MenuComponent}>
+        <View style={{flex: 1}}>
+          <NavigationViewContainer />
+          {__DEV__ && <DeveloperMenu />}
+        </View>
+       </SideMenu>
     );
   }
 });
 
 const styles = StyleSheet.create({
+  logo: {
+    width: 260,
+    height: 84
+  },
   centered: {
     flex: 1,
     alignSelf: 'center'
